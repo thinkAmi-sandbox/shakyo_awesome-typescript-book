fdf804d068bb12d089868a1c5f8d5dc5fbaf498e
thinkAmi <dev.thinkami+git@gmail.com>
2022-12-16 14:09:09 +0900

    p85 モードを追加

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -1,10 +1,17 @@
+type Mode = 'normal' | 'hard' // ユニオン型 + リテラル型で値を強制
+
 class HitAndBlow {
     private readonly answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     private answer: string[] = []  // 型が不明なため、型アノテーションが必要
     private tryCount = 0
+    private mode: Mode
+
+    constructor(mode: Mode) {
+        this.mode = mode
+    }
 
     setting() {
-        const answerLength = 3
+        const answerLength = this.getAnswerLength()
 
         while(this.answer.length < answerLength) {
             const randNum = Math.floor(Math.random() * this.answerSource.length)
@@ -17,7 +24,15 @@ class HitAndBlow {
     }
 
     async play() {
-        const inputArr = (await promptInput('「,」区切りで3つの数字を入力してください')).split(',')
+        const answerLength = this.getAnswerLength()
+
+        const inputArr = (await promptInput(`「,」区切りで${answerLength}つの数字を入力してください`)).split(',')
+
+        if (!this.validate(inputArr)) {
+            printLine('無効な入力です')
+            await this.play()
+            return
+        }
         
         const result = this.check(inputArr)
         if (result.hit !== this.answer.length) {
@@ -53,6 +68,29 @@ class HitAndBlow {
             blow: blowCount,
         }
     }
+
+    private validate(inputArr: string[]) {
+        const isLengthValid = inputArr.length === this.answer.length
+        const isAllAnswerSourceOption = inputArr.every((val) => this.answerSource.includes(val))
+        const isAllDifferentValues = inputArr.every((val, i) => inputArr.indexOf(val) == i)
+
+        return isLengthValid && isAllAnswerSourceOption && isAllDifferentValues
+    }
+
+    private getAnswerLength() {
+        switch(this.mode) {
+            case 'normal':
+                return 3
+            case 'hard':
+                return 4
+            default:
+                // modeプロパティの型が追加(very hardが追加など)された場合に
+                // caseの実装を忘れたときは、追加された型がこのdefaultに入ってくるため
+                // ここで型エラーが発生する
+                const neverValue: never = this.mode
+                throw new Error(`${neverValue}は無効なモードです`)
+        }
+    }
 }
 
 const sayHello = (name: string) => {
@@ -72,7 +110,7 @@ const promptInput = async(text: string)  => {
 }
 
 (async () => {
-    const hitAndBlow = new HitAndBlow()
+    const hitAndBlow = new HitAndBlow('normal')
     hitAndBlow.setting()
 
     await hitAndBlow.play()
