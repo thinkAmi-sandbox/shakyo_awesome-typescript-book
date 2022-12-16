32446c0d0035ef3ecdcd565c54b22700139845da
thinkAmi <dev.thinkami+git@gmail.com>
2022-12-16 13:46:43 +0900

    p73 いったんゲーム完成

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -15,6 +15,41 @@ class HitAndBlow {
             }
         }
     }
+
+    async play() {
+        const inputArr = (await promptInput('「,」区切りで3つの数字を入力してください')).split(',')
+        
+        const result = this.check(inputArr)
+        if (result.hit !== this.answer.length) {
+            // 不正解だったら続ける
+            printLine(`---\nHit: ${result.hit}\nBlow: ${result.blow}\n---`)
+            this.tryCount++
+            await this.play()
+        } else {
+            // 正解だったら終了
+            this.tryCount++
+        }
+
+        
+    }
+
+    check(input: string[]) {
+        let hitCount = 0
+        let blowCount = 0
+
+        input.forEach((val, index) => {
+            if (val === this.answer[index]) {
+                hitCount++
+            } else if (this.answer.includes(val)) {
+                blowCount++
+            }
+        })
+
+        return {
+            hit: hitCount,
+            blow: blowCount,
+        }
+    }
 }
 
 const sayHello = (name: string) => {
@@ -36,4 +71,6 @@ const promptInput = async(text: string)  => {
 (async () => {
     const hitAndBlow = new HitAndBlow()
     hitAndBlow.setting()
+
+    await hitAndBlow.play()
 })()
