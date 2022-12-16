d28d53e007fd5a466ac8bbe53b9edfa7f7f704b7
thinkAmi <dev.thinkami+git@gmail.com>
2022-12-16 13:28:58 +0900

    p66

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -1,3 +1,15 @@
+class HitAndBlow {
+    answerSource : string[]
+    answer: string[]
+    tryCount: number
+
+    constructor() {
+        this.answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        this.answer = []
+        this.tryCount = 0
+    }
+}
+
 const sayHello = (name: string) => {
     return `Hello ${name}`
 }
@@ -15,13 +27,5 @@ const promptInput = async(text: string)  => {
 }
 
 (async () => {
-    const name = await promptInput('名前を入力してください')
-    console.log(name)
-
-    const age = await promptInput('年齢を入力してください')
-    console.log(age)
-
-    process.exit()
+    const hitAndBlow = new HitAndBlow()
 })()
-
-// process.stdout.write(sayHello('Michael Jackson'))
