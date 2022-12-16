47dccfc766229bd031715b01ce8f065eb5995cbd
thinkAmi <dev.thinkami+git@gmail.com>
2022-12-16 14:34:56 +0900

    p99 モードの入力制限を追加

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -7,7 +7,7 @@ class HitAndBlow {
     private mode: Mode = 'normal'
 
     async setting() {
-        this.mode = await promptInput('モードを入力してください') as Mode
+        this.mode = await promptSelect('モードを入力してください', ['normal', 'hard']) as Mode
 
         const answerLength = this.getAnswerLength()
 
@@ -99,12 +99,32 @@ const printLine = (text: string, breakLine = true) => {
     process.stdout.write(text + (breakLine ? '\n': ''))
 }
 
+const readLine = async () => {
+    const input: string = await new Promise((resolve) => process.stdin.once('data', (data) => resolve(data.toString())))
+    return input.trim()
+}
+
 const promptInput = async(text: string)  => {
     printLine(`\n${text}\n> `, false)
 
-    const input: string = await new Promise((resolve) => process.stdin.once('data', (data) => resolve(data.toString())))
+    return readLine()
+}
 
-    return input.trim()
+const promptSelect = async (text: string, values: readonly string[]): Promise<string> => {
+    printLine(`\n${text}`)
+
+    values.forEach((value) => {
+        printLine(`- ${value}`)
+    })
+
+    printLine(`>`, false)
+
+    const input = await readLine()
+    if (values.includes(input)) {
+        return input 
+    } else {
+        return promptSelect(text, values)
+    }
 }
 
 (async () => {
