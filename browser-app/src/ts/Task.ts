8c3183413bfaee4d995895e291bdd89ad850cfcb
thinkAmi <dev.thinkami+git@gmail.com>
2022-12-17 18:29:31 +0900

    p183 enumを追加

--- browser-app/src/ts/Task.ts
+++ browser-app/src/ts/Task.ts
@@ -1,11 +1,19 @@
 import { v4 as uuid } from 'uuid'
 
+enum Status {
+    Todo = 'TODO',
+    Doing = 'DOING',
+    Done = 'DONE',
+}
+
 export class Task {
     readonly id
     title
+    status
 
     constructor(properties: {title: string}) {
         this.id = uuid()
         this.title = properties.title
+        this.status = Status.Todo
     }
 }
