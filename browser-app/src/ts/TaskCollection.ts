9c5a28c98c0616456fa65a46cf16f88e99ebadc9
thinkAmi <dev.thinkami+git@gmail.com>
2022-12-17 22:08:14 +0900

    p214 localStorageへ保存

--- browser-app/src/ts/TaskCollection.ts
+++ browser-app/src/ts/TaskCollection.ts
@@ -1,14 +1,26 @@
 import { Status, Task } from "./Task";
 
+const STORAGE_KEY = 'TASKS'
+
 export class TaskCollection {
-    private tasks: Task[] = []
+    private readonly storage
+    private tasks
+
+    constructor() {
+        // localStorageはwindowオブジェクトから直接アクセスできる
+        this.storage = localStorage
+
+        this.tasks = this.getStoredTasks()
+    }
 
     add(task: Task) {
         this.tasks.push(task)
+        this.updateStorage()
     }
 
     delete(task: Task) {
         this.tasks = this.tasks.filter(({id}) => id !== task.id)
+        this.updateStorage()
     }
 
     find(id: string) {
@@ -25,5 +37,27 @@ export class TaskCollection {
 
             return item
         })
+    }
+
+    private updateStorage() {
+        this.storage.setItem(STORAGE_KEY, JSON.stringify(this.tasks))
+    }
+
+    private getStoredTasks() {
+        const jsonString = this.storage.getItem(STORAGE_KEY)
+
+        if(!jsonString) return []
+
+        try {
+            const storedTasks: any[] = JSON.parse(jsonString)
+            const tasks = storedTasks.map((task) => new Task(task))
+
+            console.log(tasks)
+            return tasks
+        } catch {
+            this.storage.removeItem(STORAGE_KEY)
+
+            return []
+        }
     }
 }
