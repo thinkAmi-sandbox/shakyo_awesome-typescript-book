8f0173003fca073bb5ccc839aeaf07fce00f94bd
thinkAmi <dev.thinkami+git@gmail.com>
2022-12-17 20:21:07 +0900

    p185 列挙型の代替実装へと修正

--- browser-app/src/ts/Task.ts
+++ browser-app/src/ts/Task.ts
@@ -1,10 +1,11 @@
 import { v4 as uuid } from 'uuid'
 
-enum Status {
-    Todo = 'TODO',
-    Doing = 'DOING',
-    Done = 'DONE',
-}
+export const statusMap = {
+    todo: 'TODO',
+    doing: 'DOING',
+    done: 'DONE',
+} as const
+export type Status = typeof statusMap[keyof typeof statusMap]
 
 export class Task {
     readonly id
@@ -14,6 +15,6 @@ export class Task {
     constructor(properties: {title: string}) {
         this.id = uuid()
         this.title = properties.title
-        this.status = Status.Todo
+        this.status = statusMap.todo
     }
 }
