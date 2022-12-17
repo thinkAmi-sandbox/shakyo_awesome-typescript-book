0df0eb7834868c610410200fa00730f207bf8e16
thinkAmi <dev.thinkami+git@gmail.com>
2022-12-17 18:05:38 +0900

    p170 section-17

--- browser-app/src/main.ts
+++ browser-app/src/main.ts
@@ -1,1 +1,25 @@
-console.log('hello world')
+import { EventListener } from "./ts/EventListener"
+
+class Application {
+  start() {
+    const eventListener = new EventListener()
+
+    const button = document.getElementById('deleteAllDoneTask')
+
+    if (!button) return
+
+    eventListener.add(
+      'sample',
+      'click',
+      button,
+      () => alert('clicked')
+    )
+
+    eventListener.remove('sample')
+  }
+}
+
+window.addEventListener('load', () => {
+  const app = new Application()
+  app.start()
+})
