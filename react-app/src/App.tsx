0b64592d4e177b80f7ab2105408c265ebef8fd3b
thinkAmi <dev.thinkami+git@gmail.com>
2022-12-18 10:37:26 +0900

    p250 Headingコンポーネントを作成

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,8 +1,17 @@
+import { Heading } from './libs/Heading'
 import { Text } from './libs/Text'
 
 function App() {
   return (
-    <Text text={'ほげ'} />
+    <>
+      <Text text={'ほげ'} />
+
+      <Heading tag="h1">見出し</Heading>
+      <Heading tag="h1" children="childrenの見出し"></Heading>
+      <Heading tag="h1">
+        <span>hello, world!</span>
+      </Heading>
+    </>
   )
 }
 
