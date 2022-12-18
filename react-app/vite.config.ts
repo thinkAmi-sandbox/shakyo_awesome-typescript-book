ee97561c57f5a1bd2cdee29d171ae7366f52520a
thinkAmi <dev.thinkami+git@gmail.com>
2022-12-18 10:27:29 +0900

    p244 styled-componentsを適用したTextを作成

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -3,5 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      presets: ['@babel/preset-typescript'],
+      plugins: [
+        '@babel/plugin-transform-typescript',
+        [
+          'babel-plugin-styled-components',
+          {
+            ssr: false,
+            pure: true,
+            displayName: true,
+            fileName: false,
+          },
+        ],
+      ],
+    },
+  })],
 })
